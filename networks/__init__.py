13a331c8d2d256c8bebd3659acd126626aca4008
soumbane <s.banerjee@wayne.edu>
2023-06-14 15:39:55 -0400

    updated to include SwinUNETR

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,3 +1,4 @@
 from monai.networks import * # type: ignore
 from .unetr import UNETR, UNETRWithDictOutput, SelfDistillUNETRWithDictOutput
 from .nnunet import SelfDistillnnUNetWithDictOutput
+from .swinunetr import SelfDistillSwinUNETRWithDictOutput
