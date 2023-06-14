39dc512682d3479cad9d82e89954eaed0cc61061
soumbane <s.banerjee@wayne.edu>
2023-06-14 15:39:39 -0400

    updated shell scripts

--- train_sh_bk.sh
+++ train_sh_bk.sh
@@ -11,23 +11,10 @@
 ## For running train script for MMWHS Challenge Multi-class with CT ONLY (on 1 GPU): Dice CE + KL Div - Self Distillation Original
 # python train_SelfDistil_Original.py /home/share/Data/Data_MMWHS_All/CT /home/neil/Lab_work/Medical_Image_Segmentation/Dual_Self-Distillation/trained_models/last_CT_MMWHS_SelfDistil_Original_Fold1.pth --img_size 96 96 96 --batch_size 1 --epochs 600 --experiment CT_MMWHS_UNETR_SelfDist_Original_Fold1.exp --training_split 4 --device cuda:1 --show_verbose
 
+# python train_SelfDistil_Original.py /home/share/Data/Data_MMWHS_All/CT /home/neil/Lab_work/Medical_Image_Segmentation/Dual_Self-Distillation/trained_models/last_CT_MMWHS_SelfDistil_Original_Fold1.pth --img_size 96 96 96 --batch_size 1 --epochs 400 --experiment CT_MMWHS_SwinUNETR_SelfDist_Original_Fold1.exp --training_split 4 --device cuda:0 --show_verbose
+
 ## For running train script for MMWHS Challenge Multi-class with CT ONLY (on 1 GPU): Dice CE + KL Div + Boundary - Self Distillation with Distance Maps
 # python train_SelfDistil_DistMaps.py /home/neil/Lab_work/Medical_Image_Segmentation/Data_MMWHS_All/CT /home/neil/Lab_work/Medical_Image_Segmentation/Dual_SelfDistillation/trained_models/last_CT_MMWHS_SelfDistil_DistMaps.pth --img_size 96 96 96 --batch_size 1 --epochs 300 --experiment CT6_MMWHS_nnUnet_SelfDist_DistMaps.exp --training_split 4 --device cuda:1 --show_verbose
-
-#####################################################################################################################
-
-## Modality - MR
-## For running train script for MMWHS Challenge Multi-class with MR ONLY (on 1 GPU): Dice CE - Basic
-# python train_basic.py /home/neil/Lab_work/Medical_Image_Segmentation/Data_MMWHS_All/MR /home/neil/Lab_work/Medical_Image_Segmentation/Dual_SelfDistillation/trained_models/last_MR_MMWHS_Basic.pth --img_size 96 96 96 --batch_size 1 --epochs 600 --experiment MR_MMWHS_nnUnet_Basic.exp --training_split 4 --device cuda:1 --show_verbose
-
-## For running train script for MMWHS Challenge Multi-class with MR ONLY (on 1 GPU): Dice CE - Deep Supervision
-# python train_DeepSuperOnly.py /home/neil/Lab_work/Medical_Image_Segmentation/Data_MMWHS_All/MR /home/neil/Lab_work/Medical_Image_Segmentation/Dual_SelfDistillation/trained_models/last_MR_MMWHS_DeepSuperOnly.pth --img_size 96 96 96 --batch_size 1 --epochs 300 --experiment MR_MMWHS_nnUnet_DeepSuperOnly.exp --training_split 4 --device cuda:0 --show_verbose
-
-## For running train script for MMWHS Challenge Multi-class with MR ONLY (on 1 GPU): Dice CE + KL Div - Self Distillation Original
-# python train_SelfDistil_Original.py /home/neil/Lab_work/Medical_Image_Segmentation/Data_MMWHS_All/MR /home/neil/Lab_work/Medical_Image_Segmentation/Dual_SelfDistillation/trained_models/last_MR_MMWHS_SelfDistil_Original.pth --img_size 96 96 96 --batch_size 1 --epochs 300 --experiment MR_MMWHS_nnUnet_SelfDist_Original.exp --training_split 4 --device cuda:0 --show_verbose
-
-## For running train script for MMWHS Challenge Multi-class with MR ONLY (on 1 GPU): Dice CE + KL Div + Boundary - Self Distillation with Distance Maps
-# python train_SelfDistil_DistMaps.py /home/neil/Lab_work/Medical_Image_Segmentation/Data_MMWHS_All/MR /home/neil/Lab_work/Medical_Image_Segmentation/Dual_SelfDistillation/trained_models/last_MR_MMWHS_SelfDistil_DistMaps.pth --img_size 96 96 96 --batch_size 1 --epochs 300 --experiment MR_MMWHS_nnUnet_SelfDist_DistMaps.exp --training_split 4 --device cuda:1 --show_verbose
 
 
 ######################################################################################################################
